709c71458825b3d7d7b88c9953c01be9ed639aed
lauragracie <32915809+lauragracie@users.noreply.github.com>
2024-06-15 16:58:24 -0400

    Add shadow model test setup
    Added simulink model to test the sun shadow model on data from the orbit propagator.

--- shadow_model_code/is_in_shadow.m
+++ shadow_model_code/is_in_shadow.m
@@ -8,6 +8,10 @@ function is_in_shadow = is_in_shadow(earth_to_sun_unit,sat_to_earth)
     earthRadius = 6371000;
     % sat_to_earth is vector in meters
     dist_from_shadow_centre = norm(cross(sat_to_earth, earth_to_sun_unit));
-
-    is_in_shadow = (dist_from_shadow_centre <= earthRadius);
+    
+    if dist_from_shadow_centre <= earthRadius
+        is_in_shadow = 1;
+    else
+        is_in_shadow = 0;
+    end
 end
